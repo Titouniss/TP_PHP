200ec9610ba47b6e60abe4c443a01e4302e9222c
thais53 <villanne.thais@gmail.com>
2021-03-24 09:34:36 +0100

    ex3

--- index.php
+++ index.php
@@ -2,13 +2,16 @@
 $nom = "Gerard" ;
 $anneeNaissance = 2015 ;
 $age = 12;
+
 ?>
 <p>
     
     <?php 
         /*ex 1 */
         echo "Bonjour Mr  .$nom vous avez " ,2021 - $anneeNaissance ," ans".'<br>' ?>
+</p>
 
+<p>
 <?php
 /*ex 2*/
 if($age > 18)
@@ -20,3 +23,14 @@ else {
 }
 ?>
 </p>
+
+<?php
+/*ex 3 */
+$pair = 2;
+do
+{
+echo "le nombre est ". $pair . "<br/>";
+$pair= $pair+2;
+}
+while($pair <= 20);
+?>
