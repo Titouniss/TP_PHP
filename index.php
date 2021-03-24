2f85182de7038909fc57427ce509cdfbf50054f1
thais53 <villanne.thais@gmail.com>
2021-03-24 09:28:13 +0100

    ex 2

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
 $nom = "Gerard" ;
 $anneeNaissance = 2015 ;
+$age = 12;
 ?>
 <p>
+    
     <?php 
-        echo "Bonjour Mr  .$nom vous avez " ,2021 - $anneeNaissance ," ans" ?>
+        /*ex 1 */
+        echo "Bonjour Mr  .$nom vous avez " ,2021 - $anneeNaissance ," ans".'<br>' ?>
+
+<?php
+/*ex 2*/
+if($age > 18)
+{
+echo "Vous êtes majeur";
+}
+else {
+    echo "Vous êtes mineur";
+}
+?>
 </p>
